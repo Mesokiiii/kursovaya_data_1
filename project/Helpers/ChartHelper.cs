c0263435d6b22aa37e6c48d19af75ece5025e949
Developer <dev@example.com>
2026-03-12 11:13:48 +0300

    feat: улучшен ChartHelper с новым методом для позиций

--- project/Helpers/ChartHelper.cs
+++ project/Helpers/ChartHelper.cs
@@ -6,18 +6,37 @@ using OxyPlot.Axes;
 
 namespace FootballLeague
 {
+    /// <summary>
+    /// Помощник для создания графиков статистики команд
+    /// </summary>
     public class ChartHelper
     {
+        /// <summary>
+        /// Создает график изменения очков команды по турам
+        /// </summary>
+        /// <param name="history">История статистики команды</param>
+        /// <param name="teamName">Название команды</param>
+        /// <returns>Модель графика</returns>
         public static PlotModel CreatePointsChart(List<TeamRoundStats> history, string teamName)
         {
-            var model = new PlotModel { Title = $"Изменение очков: {teamName}" };
+            if (history == null || !history.Any())
+                return new PlotModel { Title = "Нет данных для отображения" };
 
+            var model = new PlotModel 
+            { 
+                Title = $"Изменение очков: {teamName}",
+                Background = OxyColors.White
+            };
+
+            // Настройка осей
             model.Axes.Add(new LinearAxis 
             { 
                 Position = AxisPosition.Bottom, 
                 Title = "Тур",
                 MajorGridlineStyle = LineStyle.Solid,
-                MinorGridlineStyle = LineStyle.Dot
+                MinorGridlineStyle = LineStyle.Dot,
+                MajorGridlineColor = OxyColor.FromRgb(230, 230, 230),
+                MinorGridlineColor = OxyColor.FromRgb(245, 245, 245)
             });
             
             model.Axes.Add(new LinearAxis 
@@ -25,9 +44,12 @@ namespace FootballLeague
                 Position = AxisPosition.Left, 
                 Title = "Очки",
                 MajorGridlineStyle = LineStyle.Solid,
-                MinorGridlineStyle = LineStyle.Dot
+                MinorGridlineStyle = LineStyle.Dot,
+                MajorGridlineColor = OxyColor.FromRgb(230, 230, 230),
+                MinorGridlineColor = OxyColor.FromRgb(245, 245, 245)
             });
 
+            // Создание серии данных
             var series = new LineSeries
             {
                 Title = teamName,
@@ -35,15 +57,70 @@ namespace FootballLeague
                 StrokeThickness = 2,
                 MarkerType = MarkerType.Circle,
                 MarkerSize = 4,
-                MarkerFill = OxyColors.Blue
+                MarkerFill = OxyColors.Blue,
+                MarkerStroke = OxyColors.DarkBlue
             };
 
+            // Добавление точек данных
             foreach (var stat in history.OrderBy(s => s.Round))
             {
                 series.Points.Add(new DataPoint(stat.Round, stat.Points));
             }
 
             model.Series.Add(series);
+            return model;
+        }
+
+        /// <summary>
+        /// Создает график сравнения позиций команд
+        /// </summary>
+        /// <param name="histories">Истории нескольких команд</param>
+        /// <returns>Модель графика</returns>
+        public static PlotModel CreatePositionChart(Dictionary<string, List<TeamRoundStats>> histories)
+        {
+            var model = new PlotModel 
+            { 
+                Title = "Изменение позиций в турнирной таблице",
+                Background = OxyColors.White
+            };
+
+            model.Axes.Add(new LinearAxis 
+            { 
+                Position = AxisPosition.Bottom, 
+                Title = "Тур"
+            });
+            
+            model.Axes.Add(new LinearAxis 
+            { 
+                Position = AxisPosition.Left, 
+                Title = "Позиция",
+                StartPosition = 1,
+                EndPosition = 0 // Инвертируем ось (1 место вверху)
+            });
+
+            var colors = new[] { OxyColors.Blue, OxyColors.Red, OxyColors.Green, OxyColors.Orange, OxyColors.Purple };
+            int colorIndex = 0;
+
+            foreach (var teamHistory in histories)
+            {
+                var series = new LineSeries
+                {
+                    Title = teamHistory.Key,
+                    Color = colors[colorIndex % colors.Length],
+                    StrokeThickness = 2,
+                    MarkerType = MarkerType.Circle,
+                    MarkerSize = 3
+                };
+
+                foreach (var stat in teamHistory.Value.OrderBy(s => s.Round))
+                {
+                    series.Points.Add(new DataPoint(stat.Round, stat.Position));
+                }
+
+                model.Series.Add(series);
+                colorIndex++;
+            }
+
             return model;
         }
     }
