88ba58b1f301acce5b4111666209371c4512eb4b
Developer <dev@example.com>
2026-03-12 11:14:31 +0300

    feat: добавлены новые конвертеры для UI

--- project/Utils/Converters.cs
+++ project/Utils/Converters.cs
@@ -1,22 +1,72 @@
+using System;
 using System.Globalization;
 using System.Windows.Data;
 
 namespace FootballLeague
 {
+    /// <summary>
+    /// Конвертер для отображения счета матча
+    /// </summary>
     public class ScoreConverter : IValueConverter
     {
         public object Convert(object value, object targetType, object parameter, CultureInfo culture)
         {
             if (value is Match match)
             {
-                return $"{match.HomeGoals} : {match.AwayGoals}";
+                if (match.IsPlayed)
+                    return $"{match.HomeGoals} : {match.AwayGoals}";
+                else
+                    return "vs";
             }
-            return "";
+            return "N/A";
         }
 
         public object ConvertBack(object value, object targetType, object parameter, CultureInfo culture)
         {
-            throw new System.NotImplementedException();
+            throw new NotImplementedException();
+        }
+    }
+
+    /// <summary>
+    /// Конвертер для форматирования позиции команды
+    /// </summary>
+    public class PositionConverter : IValueConverter
+    {
+        public object Convert(object value, object targetType, object parameter, CultureInfo culture)
+        {
+            if (value is int position)
+            {
+                return $"{position} место";
+            }
+            return "N/A";
+        }
+
+        public object ConvertBack(object value, object targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    /// <summary>
+    /// Конвертер для форматирования разности голов
+    /// </summary>
+    public class GoalDifferenceConverter : IValueConverter
+    {
+        public object Convert(object value, object targetType, object parameter, CultureInfo culture)
+        {
+            if (value is int difference)
+            {
+                if (difference > 0)
+                    return $"+{difference}";
+                else
+                    return difference.ToString();
+            }
+            return "0";
+        }
+
+        public object ConvertBack(object value, object targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
         }
     }
 }
